626e7034a51133525cccaea9accc6b2e67fbe692
Michael Shobowale <58557722+shoboske@users.noreply.github.com>
2023-08-28 17:54:31 +0300

    Clean up nav bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -19,41 +19,10 @@ export default function Navigation() {
             "fixEffect": "slide"
           }'>
 
-      <div className="container navbar-topbar">
-        <nav className="border-bottom js-mega-menu navbar-nav-wrap">
-
-          <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#topbarNavDropdown" aria-controls="topbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="d-flex justify-content-between align-items-center small">
-              <span className="navbar-toggler-text">Topbar</span>
-
-              <span className="navbar-toggler-default">
-                <i className="bi-chevron-down ms-2"></i>
-              </span>
-              <span className="navbar-toggler-toggled">
-                <i className="bi-chevron-up ms-2"></i>
-              </span>
-            </span>
-          </button>
-
-
-          <div id="topbarNavDropdown" className="navbar-nav-wrap-collapse collapse navbar-collapse navbar-topbar-collapse">
-            <div className="navbar-toggler-wrapper">
-              <div className="navbar-topbar-toggler d-flex justify-content-between align-items-center">
-                <span className="navbar-toggler-text small">Topbar</span>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topbarNavDropdown" aria-controls="topbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <i className="bi-x"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-
       <div className="container">
-        <nav className="js-mega-menu navbar-nav-wrap">
+        <nav className="js-mega-menu navbar-nav-wrap border-bottom">
           <a className="navbar-brand fw-semibold navbar-brand-logo" href="/" aria-label="Seun Owoyemi">
-            <img src="./favicon.ico" />
+            <img src="./favicon.ico" alt="site logo" />
           </a>
 
           <div className="collapse navbar-collapse pt-2" id="navbarNavDropdown">
